b0f5161e84cfdc92c6ef2fd403d57a253b416d27
rgoomes <ricardo.jpg@hotmail.com>
2014-11-29 20:22:49 +0000

    Small fix

--- src/login/action/UserAction.java
+++ src/login/action/UserAction.java
@@ -1,14 +1,11 @@
 package login.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-
 import org.apache.struts2.interceptor.SessionAware;
-
 import java.util.Map;
-
 import login.model.UserBean;
 
-public class UserAction {
+public class UserAction extends ActionSupport implements SessionAware {
 	private static final long serialVersionUID = 1L;
 	private String username = null;
 	private Map<String, Object> session;
