9987c6b91a3279dca4c40f0a9684697aff4921d4
rgoomes <ricardo.jpg@hotmail.com>
2014-12-01 17:53:22 +0000

    Small fix

--- src/login/action/KeyAction.java
+++ src/login/action/KeyAction.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import login.model.KeyBean;
 
-public class KeyAction {
+public class KeyAction extends ActionSupport implements SessionAware {
 	private static final long serialVersionUID = 1L;
 	private Map<String, Object> session;
 	private String description = null;
@@ -17,9 +17,11 @@ public class KeyAction {
 	public String execute() {
 		String meeting_id = session.get("meeting_id").toString();
 		String item_id = session.get("item_id").toString();
-		if(!meeting_id.equals("-1") && !item_id.equals("-1")) {
+		if(!meeting_id.equals("-1") && !item_id.equals("-1") && description != null && !description.equals("")) {
 			this.getKeyBean().newKeyDecision(item_id, description);
 		}
+		
+		return SUCCESS;
 	}
 	
 	public void setDescription(String description) {
